16542185f9e9de807f633dee21d3c4dde4f5fd74
Puun2545 <puun.vach@gmail.com>
2025-03-12 20:15:28 +0700

    v2.0.0

---
+++ src/public/script.js
@@ -0,0 +1,3 @@
+document.getElementById("clickMe").addEventListener("click", () => {
+    alert("I Love U 💖");
+});
--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello from Node.js on Kubernetes! I Luv U <33 Tag 1.0.3");
-});
+// ให้ Express เสิร์ฟไฟล์ใน public/
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
